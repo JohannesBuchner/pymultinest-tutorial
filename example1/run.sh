f2a2d9bd6e26d08391965e8fc95d7af529903ec1
Johannes Buchner <johannes.buchner.acad@gmx.com>
2013-11-11 16:06:45 +0100

    some annotations

--- example1/run.sh
+++ example1/run.sh
@@ -1,9 +1,8 @@
-
+# run the script
 python 1d_multimodal.py
-
+# analyse the output, which is in out/.
+# multinest_marginals.py is a plotting tool that comes with pymultinest.
 multinest_marginals.py out/
-
+# open the marginal plots at marg.pdf
 xdg-open out/marg.pdf
-
-
 
